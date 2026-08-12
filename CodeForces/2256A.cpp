f84d086e25979ec10443ec1f30ded4cdb5b421fe
Siddharth <92183089+Siddharth-Habbu@users.noreply.github.com>
2026-08-12 14:47:25 +0530

    11/08

---
+++ CodeForces/2256A.cpp
@@ -0,0 +1,70 @@
+/*
+Written & Produced By: Czhearth
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using l = long;
+using pii = pair<int,int>;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+
+ll gcdll(ll a, ll b){
+    return b ? gcdll(b, a % b) : a;
+}
+
+ll lcmll(ll a, ll b){
+    return a / gcdll(a, b) * b;
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+       int x,y,z,m1,m2,m3;
+       cin >> x >> y >> z; 
+        m1 = min(x,y);
+        if (m1 == x){
+            m2 = min(y,z);
+            m3 = max(y,z);
+        }
+        else{
+            m2 = min(x,z);
+            m3 = max(x,z);
+        }
+
+        if(m3 <= m1+m2){
+            cout << m3 - min(m1,m2) << "\n";
+        }
+        else{
+            m3 = m1+m2;
+            cout << m3 - min(m1,m2) << "\n";
+        }
+
+
+       
+    }
+
+
+    return 0;
+}
